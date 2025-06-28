e60b9a986d02372145a2448a8292c150bf5bda9d
Vandaao <vandorpmg@gmail.com>
2025-06-28 10:18:12 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Coopervetra - Gerenciador de Fretes',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
